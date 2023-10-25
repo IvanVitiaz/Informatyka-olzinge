78740dab11a00abbfacf19a0c2ae796cfbac7c70
IvanVitiaz <vityaz.ivan.school@gmail.com>
2023-10-25 15:03:46 +0200

    Obnowa

--- Olzinge shieksynnsyn.cpp
+++ Olzinge shieksynnsyn.cpp
@@ -1,9 +1,36 @@
 #include <iostream> 
 using namespace std;
 	int main(){
-	short zmienna = 2;
-	cout << "Olzinge sieksynsyn";
-	cout <<  endl;
-	cout << "Liczba: " << zmienna;
+	
+	short liczba = 2;
+	float zmiennoprzecinkowa = 3.21;
+	string tekst = "tekst";
+	char znak = 'A';
+	bool logiczny = false;
+	/*
+	cout << "Hello world";
+	cout << endl; // koniec lini
+	cout << "Liczba: " << liczba; // wyswietlenie zmiennej
+	cout << endl;
+	
+	string wyraz;
+	cout << "Podaj jakis wyraz: ";
+	cin >> wyraz;
+	cout << "Podales wyraz: " <<  wyraz;
+	*/
+	string imie;
+	cout << "Podaj swoje imie: ";
+	cin >> imie;
+	cout << "Imie: " << imie;
+	cout << endl;
+	
+	short wiek;
+	cout << "Podaj wiek: ";
+	cin >> wiek;
+	cout << "Podales wiek " << wiek;
+	cout << endl;
+	
+	cout << "Witaj " << imie << " masz " << wiek << " lat !" << endl;
 	return 0;
+	
 }
